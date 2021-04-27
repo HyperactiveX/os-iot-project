4175207edd4e2cfb5eaae49485b61157e0d877c7
HyperactiveX <s.chalanthorn@gmail.com>
2021-04-27 21:58:35 +0700

    Chore : Fix database problem

--- server/src/index.js
+++ server/src/index.js
@@ -1,42 +1,57 @@
 var cors = require('cors');
-const connection = require('./DBconnection')
-const express = require('express')
-
+const express = require('express');
+const db = require('./DBconnection');
+// const database = require('./DBconnection');
 
 const app = express()
+
 var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // For legacy browser support
 }
 app.use(cors(corsOptions));
 
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
-    connection.connect()
-
-    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    if (err) throw err
-
-    console.log('The solution is: ', rows[0].solution)
+    res.send({
+        success : true
     })
-
-    connection.end()
-    res.send(
-        {
-            test : "success",
-            db : rows[0].solution
-        }
-    )
 })
 
-app.get("/authenticate", (req, res) => {
-    const username = req.query.username
-    const password = req.query.password
-    
-    if (username == 'test' && password == 'test') {
-        return res.send(true)
+app.get("/take", async (req, res) => {
+    try {
+        const result = await db.execute(`SELECT * FROM USERS`)
+        console.log(result)
+        // res.send(result)
+        db.end()
+        
+    } catch (err) {
+        console.log(err)
     }
-    return res.send(false)
+
 })
+
+// app.post("/authenticate", (req, res) => {
+//     const username = req.query.username
+//     const password = req.query.password
+    
+//     if (username && password) {
+//         try {
+//             db.promise().query(`SELECT uuid FROM Users where username = '${username}'`);
+//             res.status(201).send({msg: "User is in the database"})
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+//     return res.send(false)
+// })
 
 app.listen(8080, "0.0.0.0", () => {
     console.log('Started at port 8080');
